a159f9b39d3b2a732f8e55c81fdc777493e678e5
StanislavSolovei <edukacjateb176@gmail.com>
2026-04-28 10:40:35 +0200

    Add files via upload

---
+++ Zadanie2/script.js
@@ -0,0 +1,13 @@
+const tekst = document.getElementById("text");
+
+function red (){
+    tekst.style.color = "red";
+}
+
+function blue (){
+    tekst.style.color = "blue";
+}
+
+function green (){
+    tekst.style.color = "green";
+}
